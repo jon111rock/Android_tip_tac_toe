178e9cf0b57a64fd3655ac3ff208f496da963244
薛猷騰 <jon111rock@gmail.com>
2021-03-05 19:40:09 +0800

    setup imageView onClickListener

--- app/src/main/java/com/uton/tbdconnect3/MainActivity.java
+++ app/src/main/java/com/uton/tbdconnect3/MainActivity.java
@@ -3,8 +3,13 @@ package com.uton.tbdconnect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initalize imgView array
+        ImageView[][] imgArray = new ImageView[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String imgId = "imageView_" + i + j;
+                imgArray[i][j] = findViewById(getResources().getIdentifier(imgId, "id", getPackageName()));
+                imgArray[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(TAG, "onClick: " + getResources().getResourceName(v.getId()));
+                    }
+                });
+            }
+        }
+
+
     }
 }
